890cdcfa8a8af54c3ecc8cea9235c8b569dd771d
Logan M <42723034+YesImSevy@users.noreply.github.com>
2021-12-13 17:03:17 -0800

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,11 @@
+const Settings = {
+    token: '',
+    users: ['USER_ID'],
+    botId: '',
+    // APPEARANCE
+    name: 'Name Here',
+    logo: 'Image URL',
+    color: '#12E29B'
+}
+
+module.exports = Settings
